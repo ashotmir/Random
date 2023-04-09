1553880981f44ac6e34d7e068f4c73af18220735
Ashot <ashormir00@gmail.com>
2023-04-09 17:00:18 +0400

    HW

--- Library.py
+++ Library.py
@@ -1,1 +1,59 @@
-1
+class Book:
+    def __init__(self,title, authors, year, ISBN, genre, copy=1) -> None:
+        pass
+
+    def add_new_copy():
+        pass
+    
+    def available_copy():
+        pass 
+
+class Book_copy:
+    def __init__(self, rating,) -> None:
+        pass
+
+    def borrow_copy():
+        pass
+    
+    def return_copy():
+        pass
+
+    def change_rating():
+        pass
+
+class Student:
+    def __init__(self,name, ID, email, books,borrowing_limit=5) -> None:
+        pass
+
+    def borrow_book():
+        pass
+
+    def return_book():
+        pass
+
+class Library(Book,Student):
+    pass
+
+    def add_book():
+        pass
+
+    def remove_book():
+        pass
+    
+    def add_student():
+        pass
+
+    def remove_student():
+        pass
+
+    def change_borrowing_limit():
+        pass
+
+    def all_books():
+        pass
+
+    def available_books():
+        pass
+
+    def search_book():
+        pass
